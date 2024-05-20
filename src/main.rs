fda8318666a658b9c8eadfb522719dd08c51aac2
Ross Baquir <streakinthesky@gmail.com>
2024-05-20 12:39:02 -0700

    read file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::io::BufReader;
 use std::thread;
+use std::fs;
 
 const STATUS_200: &str = "HTTP/1.1 200 OK";
 const STATUS_404: &str = "HTTP/1.1 404 Not Found";
@@ -38,33 +39,59 @@ fn get_user_agent(buffer: &mut BufReader<&mut TcpStream>) -> String {
     response.join("\r\n")
 }
 
+fn get_file(filename: &str, dir: &str) -> std::io::Result<String> {
+    let filepath = String::new() + dir + "/" + filename;
+    let file = fs::read(filepath)?;
+    let headers = format!("Content-Type: application/octet-stream\r\nContent-Length: {}\r\n", file.len());
+    let response = [STATUS_200, &headers, &String::from_utf8(file).unwrap()];
+    Ok(response.join("\r\n"))
+}
+
+fn get_dir(mut args: std::env::Args) -> String {
+    for arg in args.by_ref()  {
+        if arg == "--directory"{
+            break;
+        }
+    };
+
+    if let Some(directory) = args.next() {
+        return directory;
+    }
+    String::from("/")
+}
+
+
 fn main() {
-     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-     println!("Listening on port 4221");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("Listening on port 4221");
 
-     for stream in listener.incoming() {
-         thread::spawn(|| {
-             match stream {
-                 Ok(mut stream) => {
-                     let mut buffer = BufReader::new(&mut stream);
-                     let mut req_line_string = String::new();
-                     let _ = buffer.read_line(&mut req_line_string).unwrap();
-                     let url = get_request_url(&req_line_string);
-                     let url_parts: Vec<&str> = url.split_terminator('/').collect();
+    for stream in listener.incoming() {
+        thread::spawn(move|| {
+            match stream {
+                Ok(mut stream) => {
+                    let mut buffer = BufReader::new(&mut stream);
+                    let mut req_line_string = String::new();
+                    let _ = buffer.read_line(&mut req_line_string).unwrap();
+                    let url = get_request_url(&req_line_string);
+                    let url_parts: Vec<&str> = url.split_terminator('/').collect();
+                    println!("{:?}", url_parts);
+                    let response = match url_parts[..] {
+                        [""] => STATUS_200.to_string() + "\r\n\r\n",
+                        ["", "echo", endpoint] => echo(endpoint),
+                        ["", "user-agent"] => get_user_agent(&mut buffer),
+                        ["", "files", filename] => match get_file(filename, &get_dir(std::env::args())) {
+                            Ok(response) => response,
+                            _ => STATUS_404.to_string() + "\r\n\r\n",
+                        },
+                        _ => STATUS_404.to_string() + "\r\n\r\n",
+                    };
 
-                     let response = match url_parts[..] {
-                         [""] => STATUS_200.to_string() + "\r\n\r\n",
-                         ["", "echo", endpoint] => echo(endpoint),
-                         ["", "user-agent"] => get_user_agent(&mut buffer),
-                         _ => STATUS_404.to_string() + "\r\n\r\n",
-                     };
-
-                     let _ = stream.write(response.as_bytes());
-                 }
-                 Err(e) => {
-                     println!("error: {}", e);
-                 }
-             }
-         });
-     }
+                    let _ = stream.write(response.as_bytes());
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
+            }
+        });
+    }
 }
